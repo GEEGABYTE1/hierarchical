722ffdbff4b7275f0cc02545907edb8ee35ddadf
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-06-24 12:51:36 -0400

    Bug Fixes

--- dfs.py
+++ dfs.py
@@ -1,13 +1,13 @@
 from tree import TreeNode
 
-def search_name(root_node, goal_value, path):
+def search_name(root_node, goal_value, path=()):
     path = path + (root_node,)
 
     if root_node.value == goal_value:
         return path 
     
     for child in root_node.children:
-        path_found = dfs(child, goal_value, path)
+        path_found = search_name(child, goal_value, path)
 
         if not path_found == None:
             return path_found 
